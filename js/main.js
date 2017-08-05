8cfca2f3fe2d8be190fc81839e9526a8dc04175c
itujono <itujono@gmail.com>
2017-08-06 01:37:27 +0700

    Lightbox/modal heheh

--- js/main.js
+++ js/main.js
@@ -1,18 +1,39 @@
 
-var carousel = document.querySelector('.carousel');
-var flkty = new Flickity( carousel, {
-    imagesLoaded: true,
-    pageDots: true,
-});
 
-tippy('.social a', {
-    followCursor: true,
-    position: 'bottom'
-});
+$(function() {
 
-tippy('section > h4', {
-    position: 'top',
-    followCursor: true,
-    size: 'small',
-    inertia: true
+    var carousel = document.querySelector('.carousel');
+    var flkty = new Flickity( carousel, {
+        imagesLoaded: true,
+        pageDots: true,
+    });
+
+    tippy('.social a', {
+        followCursor: true,
+        position: 'bottom'
+    });
+
+    tippy('section > h4', {
+        position: 'top',
+        followCursor: true,
+        size: 'small',
+        inertia: true
+    });
+
+    var modal = new tingle.modal({
+        footer: false,
+        onOpen: function() {
+            console.log("Try sikit ya heheh");
+        }
+    });
+
+    var btn = document.querySelector('#modaal');
+    var first = document.querySelector('#first');
+
+    modal.setContent(first.innerHTML);
+
+    btn.addEventListener('click', function() {
+        modal.open();
+    });
+
 });
