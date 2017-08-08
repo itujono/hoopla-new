4a286250e09a0791861b569b04ef1bdeccf7c92f
itujono <itujono@gmail.com>
2017-08-08 15:34:28 +0700

    Form validation bikin binun.

--- js/main.js
+++ js/main.js
@@ -17,3 +17,18 @@
         size: 'small',
         inertia: true
     });
+
+    var cart = document.querySelector(".cart");
+    var btn = document.querySelector("#btn-cart");
+
+    function showCart() {
+        if (!cart.classList.contains("clicked")) {
+            cart.classList.add("clicked");
+            btn.classList.add("moved");
+        } else {
+            cart.classList.remove("clicked");
+            btn.classList.remove("moved");
+        }
+    }
+
+    btn.addEventListener("click", showCart);
