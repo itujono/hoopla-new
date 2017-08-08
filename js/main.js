22184f68e4c5e7eb2bc3a482bdcf3cfbb43245ff
itujono <itujono@gmail.com>
2017-08-08 23:55:43 +0700

    Button cart di Discount ni agak-agak sikit.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var btn = document.querySelector("#btn-cart");
 ////////////////////////////////////
 
 
+
+
+
 tippy('.social a', {
     followCursor: true,
     position: 'bottom'
@@ -21,12 +24,12 @@ function showCart() {
     if (!cart.classList.contains("clicked")) {
         cart.classList.add("clicked");
         btn.classList.add("moved");
+        console.log("Sempardak cart");
     } else {
         cart.classList.remove("clicked");
         btn.classList.remove("moved");
     }
 }
-
 
 btn.addEventListener("click", showCart);
 
@@ -34,11 +37,21 @@ btn.addEventListener("click", showCart);
 
 
 
+// Tingle's Event Listeners
+document.querySelector('#modaal').addEventListener('click', function() { modal.open(); });
+document.querySelector('#modaal-second').addEventListener('click', function() { modalSecond.open(); });
+
+// Tingle's setContent
+modal.setContent(document.querySelector('#first').innerHTML);
+modalSecond.setContent(document.querySelector('#second').innerHTML);
 
 
-
-
+// Flickity
 var flkty = new Flickity( carousel, {
     imagesLoaded: true,
     pageDots: true,
 });
+
+// Declare Tingle's vars
+var modal = new tingle.modal({ cssClass: ['modaler'] });
+var modalSecond = new tingle.modal({ cssClass: ['modaler'] });
