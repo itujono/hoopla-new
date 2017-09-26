38d5520ad9e37fb646847dddc9ad11417a23803a
andhan <magicwarms@gmail.com>
2017-09-26 10:30:48 +0700

    Ketinggalan wak!

--- application/controllers/About.php
+++ application/controllers/About.php
@@ -5,6 +5,7 @@ class About extends Frontend_Controller {
 
 	public function index() {
 		$data['addONS'] = 'about';
+		$data['title'] = 'About - Toys Rent';
 		$data['idbody'] = 'about';
 		$data['subview'] = $this->load->view($this->data['frontendDIR'].'about', $data, TRUE);
         $this->load->view($this->data['rootDIR'].'_layout_base_frontend',$data);
