d70a83f1fca126690d30784a8e8f0dd2bbf8f949
andhan <magicwarms@gmail.com>
2017-10-02 10:41:16 +0700

    Function untuk email

--- application/controllers/E1.php
+++ application/controllers/E1.php
@@ -10,4 +10,28 @@ class E1 extends Frontend_Controller {
 		$this->load->view($this->data['frontendDIR'].'e1');
         // $this->load->view($this->data['rootDIR'].'_layout_base_frontend',$data);
 	}
+
+	public function e2() {
+		// $data['addONS'] = 'about';
+		// $data['title'] = 'Pesanan datang!';
+		// $data['idbody'] = 'e1';
+		$this->load->view($this->data['frontendDIR'].'e2');
+        // $this->load->view($this->data['rootDIR'].'_layout_base_frontend',$data);
+	}
+
+	public function e3() {
+		// $data['addONS'] = 'about';
+		// $data['title'] = 'Pesanan datang!';
+		// $data['idbody'] = 'e1';
+		$this->load->view($this->data['frontendDIR'].'e3');
+        // $this->load->view($this->data['rootDIR'].'_layout_base_frontend',$data);
+	}
+
+	public function e4() {
+		// $data['addONS'] = 'about';
+		// $data['title'] = 'Pesanan datang!';
+		// $data['idbody'] = 'e1';
+		$this->load->view($this->data['frontendDIR'].'e4');
+        // $this->load->view($this->data['rootDIR'].'_layout_base_frontend',$data);
+	}
 }
