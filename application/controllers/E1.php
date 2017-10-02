c2932fb9450c5aa208c1ca511284dfe77f4ac4bd
andhan <magicwarms@gmail.com>
2017-10-02 10:12:15 +0700

    betulin punya riva

--- application/controllers/E1.php
+++ application/controllers/E1.php
@@ -4,10 +4,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class E1 extends Frontend_Controller {
 
 	public function index() {
-		$data['addONS'] = 'about';
-		$data['title'] = 'Pesanan datang!';
-		$data['idbody'] = 'e1';
-		$data['subview'] = $this->load->view($this->data['frontendDIR'].'e1', $data, TRUE);
-        $this->load->view($this->data['rootDIR'].'_layout_base_frontend',$data);
+		// $data['addONS'] = 'about';
+		// $data['title'] = 'Pesanan datang!';
+		// $data['idbody'] = 'e1';
+		$this->load->view($this->data['frontendDIR'].'e1');
+        // $this->load->view($this->data['rootDIR'].'_layout_base_frontend',$data);
 	}
 }
